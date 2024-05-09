d750fb0be698b7803d1e628fcf2e85a60a8ff4fc
SteveLorde <mostafa.maher98@gmail.com>
2024-05-09 20:26:37 +0200

    some functions in map component

--- components/map/map.tsx
+++ components/map/map.tsx
@@ -1,15 +1,41 @@
-import MapView from "react-native-maps";
+import MapView, {LatLng, LongPressEvent, Region} from "react-native-maps";
 import {StyleSheet} from "react-native";
-import {useRef} from "react";
+import {useRef, useState} from "react";
+import * as Location from 'expo-location';
 
 //THIS MAP ENTIRE MAP COMPONENT IS CONSIDERED A PLACEHOLDER PRESENTATION
 
+interface MapMarker {
+    coordinate : LatLng,
+    title : string
+}
+
 export default function Map() {
+    //const [markers, setMarkers] = useState<MapMarker[]>([])
+    const [destinationMarker, setDestinationMarker] = useState<MapMarker>()
 
     const mapRef = useRef(null)
 
+    function TouchMark(touchEvent : LongPressEvent) {
+        const {coordinate} = touchEvent.nativeEvent
+        const newMarker : MapMarker = {
+            coordinate: coordinate,
+            title: 'Destination'
+        }
+        setDestinationMarker(newMarker)
+    }
+
+    function MapDrag(event : Region) {
+
+    }
+
+    function MapDragDone(eventRegion : Region) {
+
+    }
+
+
     return <>
-        <MapView ref={mapRef} style={mapElementStyle.mapview} showsUserLocation={true}  />
+        <MapView ref={mapRef} style={mapElementStyle.mapview} showsUserLocation={true} onRegionChange={(event) => MapDrag(event)} onRegionChangeComplete={(event) => MapDragDone(event)} onLongPress={(event) => TouchMark(event)}  />
     </>
 }
 
