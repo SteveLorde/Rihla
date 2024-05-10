dd3db7412278c5f770ae9a730fe9eacdf32a415a
SteveLorde <mostafa.maher98@gmail.com>
2024-05-10 17:08:51 +0200

    login and register functions in index.tsx (login page)

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,39 @@
 import {GlobalStyle} from "../styles/GlobalStyle"
 import {SafeAreaView, TextInput, View, Text, StyleSheet, TouchableOpacity} from "react-native";
 import {Controller, useForm} from "react-hook-form";
-import {useState} from "react";
+import {useContext, useState} from "react";
 import {LoginRequest} from "@/data/models/LoginRequest";
 import {RegisterRequest} from "@/data/models/RegisterRequest";
+import {MainContext} from "@/services/state/maincontext";
+import { router } from 'expo-router';
+
 
 
 export default function Page() {
     const { control : loginform, handleSubmit : loginformsubmit, formState: { errors : loginformerrors } } = useForm<LoginRequest>();
     const { control : registerform, handleSubmit : registerformsubmit, formState: { errors : registerformerrors } } = useForm<RegisterRequest>();
     const [isToggleForm, setToggleForm] = useState(false)
+    const {authService} = useContext(MainContext)
 
 
-    function Login() {
-
+    async function Login(loginReq : LoginRequest) {
+        const check = await authService.Login(loginReq)
+        if (check) {
+            router.navigate("/dashboard")
+        }
+        else {
+            //to implement modal popup
+        }
     }
 
-    function Register() {
-
+    async function Register(registerReq : RegisterRequest) {
+        const check = await authService.Register(registerReq)
+        if (check) {
+            router.navigate("/dashboard")
+        }
+        else {
+            //to implement modal popup
+        }
     }
 
 
